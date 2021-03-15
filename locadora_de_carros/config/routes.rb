42978d5caf420b9d044283ba2ffe65f1663a6d80
quemia <que.carol@hotmail.com>
2021-03-15 13:58:53 -0300

    Alterações Html

--- locadora_de_carros/config/routes.rb
+++ locadora_de_carros/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
+  resources :registers
   devise_for :users
   #get 'home/index'
   get 'home/sobre'
   root 'home#index'
+  get 'home/register'
 
 end
--- locadora_de_carros/db/schema.rb
+++ locadora_de_carros/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_191514) do
+ActiveRecord::Schema.define(version: 2021_03_14_202615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "registers", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.integer "cpf"
+    t.datetime "born"
+    t.string "tellphone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
